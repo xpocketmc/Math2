d9dfa482f9bdaef91c7ba727402ac154f47f2bc2
XPocketMC <tvfinitoflamboyan@gmail.com>
2024-09-20 13:56:20 +0700

    Vector4 Implementation

---
+++ src/Vector4.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace pocketmine\math;
+
+class Vector4 {
+  private $vector4;
+
+  public function __construct(int $vector4){
+    $this->vector4 = $vector4;
+  }
+
+  public function Vector4() {
+    //NOOP
+  }
