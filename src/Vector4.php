c88f13833e69ab721f3af3c406dde7b0567e3b78
XPocketMC <tvfinitoflamboyan@gmail.com>
2024-09-20 14:04:54 +0700

    .

--- src/Vector4.php
+++ src/Vector4.php
@@ -3,13 +3,35 @@
 namespace pocketmine\math;
 
 class Vector4 {
-  private $vector4;
+    private $vector4;
 
-  public function __construct(int $vector4){
-    $this->vector4 = $vector4;
-  }
+    public function __construct(int $vector4) {
+        $this->vector4 = $vector4;
+    }
 
-  public function Vector4() {
-    //NOOP
-  }
+    public function setVector4(int $value): void {
+        $this->vector4 = $value;
+    }
+
+    public function getVector4(): int {
+        return $this->vector4;
+    }
+
+    public function add(int $value): int {
+        $this->vector4 += $value;
+        return $this->vector4;
+    }
+
+    public function double(): int {
+        $this->vector4 *= 2;
+        return $this->vector4;
+    }
+
+    public function __toString(): string {
+        return "Vector4: " . $this->vector4;
+    }
+
+    public function equals(Vector4 $other): bool {
+        return $this->vector4 === $other->getVector4();
+    }
 }
